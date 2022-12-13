bebfb1e4d2f84e443379562be3ebaf5c6de2bee0
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2022-12-13 11:03:56 -0600

    Added notes and starter code

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
 // Write your JavaScript code here!
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
 
-   let listedPlanets;
-   // Set listedPlanetsResponse equal to the value returned by calling myFetch()
-   let listedPlanetsResponse;
-   listedPlanetsResponse.then(function (result) {
-       listedPlanets = result;
-       console.log(listedPlanets);
-   }).then(function () {
-       console.log(listedPlanets);
-       // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
-   })
-   
+    // let listedPlanets;
+    // // Set listedPlanetsResponse equal to the value returned by calling myFetch()
+    // let listedPlanetsResponse;
+    // listedPlanetsResponse.then(function (result) {
+    //     listedPlanets = result;
+    //     console.log(listedPlanets);
+    // }).then(function () {
+    //     console.log(listedPlanets);
+    //     // Below this comment call the appropriate helper functions to pick a planet fom the list of planets and add that information to your destination.
+    // })
+
+    let form = document.getElementById("launchForm");
+
+    form.addEventListener("submit", function (event) {
+        event.preventDefault();
+        // Any code in here will execute after the form submits
+        // To make this file more organized, we are going move any code we put in here into the formSubmission
+        // function in scriptHelper.js
+        // That way, we can call that formSubmission function here, and reduce the amount of code in this file
+
+        // We have to understand what needs to be passed in to the formSubmission function
+        // The document is just the document object, we need to pass it in to be able to use the document object
+        // in the actual formSubmission function
+        // As far as the pilot, copilot, fuelLevel, and the cargoMass, we need to select these inputs using the DOM
+        // and pass in the values of these inputs into the formSubmission function
+        let pilotInput = document.getElementById("pilotName");
+        // Since the copilot input in our form doesn't have an id, you either use a querySelector or
+        // you could add an id attribute yourself to the input so you can use getElementById
+        // let copilotInput = document.getElementById("copilotName");
+
+        // Alongside selecting all of the inputs from the form, we also want to select the faultyItems div
+        // and pass into the formSubmission function
+        let list = document.getElementById("faultyItems");
+
+        formSubmission(document, list, pilotInput.value, copilotInput.value, fuelLevelInput.value, cargoMassInput.value);
+    })
+
 });
--- scriptHelper.js
+++ scriptHelper.js
@@ -2,33 +2,39 @@
 require('isomorphic-fetch');
 
 function addDestinationInfo(document, name, diameter, star, distance, moons, imageUrl) {
-   // Here is the HTML formatting for our mission target div.
-   /*
-                <h2>Mission Destination</h2>
-                <ol>
-                    <li>Name: </li>
-                    <li>Diameter: </li>
-                    <li>Star: ${star}</li>
-                    <li>Distance from Earth: </li>
-                    <li>Number of Moons: </li>
-                </ol>
-                <img src="">
-   */
+    // Here is the HTML formatting for our mission target div.
+    /*
+                 <h2>Mission Destination</h2>
+                 <ol>
+                     <li>Name: </li>
+                     <li>Diameter: </li>
+                     <li>Star: ${star}</li>
+                     <li>Distance from Earth: </li>
+                     <li>Number of Moons: </li>
+                 </ol>
+                 <img src="">
+    */
 }
 
 function validateInput(testInput) {
-   
+    // Take in a value from (from one of the form inputs) and returns information about whether 
+    // the input is valid or not
+    // If the value passed in to this function is empty, we want to return "Empty"
+    // Else if the value is not a number, we return "Not a Number"
+    // Else if the value IS a number, we should return "Is a Number"
+    // isNaN(testInput) will return true if testInput is NOT a number
+    // isNaN("1000") ---> false
 }
 
 function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
-   
+
 }
 
 async function myFetch() {
     let planetsReturned;
 
-    planetsReturned = await fetch().then( function(response) {
-        });
+    planetsReturned = await fetch().then(function (response) {
+    });
 
     return planetsReturned;
 }
@@ -39,5 +45,5 @@ function pickPlanet(planets) {
 module.exports.addDestinationInfo = addDestinationInfo;
 module.exports.validateInput = validateInput;
 module.exports.formSubmission = formSubmission;
-module.exports.pickPlanet = pickPlanet; 
+module.exports.pickPlanet = pickPlanet;
 module.exports.myFetch = myFetch;
