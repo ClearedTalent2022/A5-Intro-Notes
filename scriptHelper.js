e8eb78fbbcb04848f4bb964391ff3d7024cfe3dd
Pearce Odegard <69996490+podegard89@users.noreply.github.com>
2022-12-14 12:42:52 -0600

    Added more notes on updating launch checklist via DOM

--- scriptHelper.js
+++ scriptHelper.js
@@ -54,6 +54,29 @@ function formSubmission(document, list, pilot, copilot, fuelLevel, cargoLevel) {
     // else if any of the fields have the wrong type data in them, send an alert to the user to input valid data types
     // else, in the case we pass both of these validation steps, we can begin updating the Launch Information section
 
+    // Select the pilotStatus and copilotStatus HTML elements so that we can update them using the DOM
+    // Once selected, we can add in the text showing that both our pilot and copilot are ready for launch
+    // We also need to get the other couple of launch checklist HTML elements that we are updating below (launchStatus, 
+    // fuelStatus, cargoStatus)
+
+    // Looks like we've also determined that in any of the possible cases for fuel level and cargo mass,
+    // the faultyItems list's visibility needs to change to "visible"
+    // Remember that unlike all the HTML elements we selected above, we already selected the faultyItems list over in 
+    // script.js, and passed it in as an argument when we called the formSubmission function there
+
+
+    // Given that we are updating the launch checklist based on the fuel level AND the cargo mass, let's plot out
+    // all possible outcomes from the from
+    // - fuel level is too low AND the cargo mass is too high
+    //      - launchStatus text should read "Shuttle not ready for launch"
+    //      - launchStatus text color should change to red
+    //      - fuelStatus text should read "Fuel level is too low for launch"
+    //      - cargoStatus text should read "Cargo mass is too high for launch"
+    // - fuel level is too low AND the cargo mass is low enough
+    // - fuel level is high enough AND the cargo mass is low enough
+    // - fuel level is high enough AND the cargo mass is too high
+
+
 
 }
 
